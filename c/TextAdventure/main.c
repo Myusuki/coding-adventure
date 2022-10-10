bdcde78ce30d1016ac4c6bf40f98f63dd5a9df4e
Myusuki <myusuki_86666@tutanota.com>
2022-10-10 16:28:19 -0400

    C learning projects

---
+++ c/TextAdventure/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf( "Beep! Beep! Beep!" );
+	printf( "It's a Saturday morning. You wake up, reluctant to get out of bed." );
+	printf( "The sun shines brightly through your berdroom window." );
+	printf( "Going down stairs you go perform your same old boring morning routine." );
+	printf( "As usual, every thing is done with no fuss and ease." );
+	printf( "You go back to your room to finish your homework that you've pushed off for today." );
+	printf( "By noon you finish your homework and had lunch. With nothing to do you decide to go out and bike." );
+
+	printf( "Going to the park, you bike around with the wind blowing in your face pleasantly." );
+	printf( "Not feeling the want to bike anymore, you seat yourself at a nearby bench to rest." );
+	printf( "You bathe in the sun, enjoying yourself." );
+	printf( "2 ladies walk by, speaking of some rumor." );
+	printf( "You hear them talk about strange things happening at mountain." );
+	printf( "Being the daredevil you are, you decide tomorrow to go explore exactly what's going on." );
+
+	printf( "Beep! Beep! Beep!" );
+	printf( "It's Sunday morning. Today is somewhat cloudy." );
+	printf( "You get out of bed, eager to go explore the mountain. You perform your same old boring morning routine with haste." );
+	printf( "With the morning routine out of the way, you start packing your backpack meticulously. Prepping little but well enough for your spontaneous journey." );
+	printf( "Arriving at the mountain by bike, nothing seems to be out of the ordinary." );
+	printf( "Taking a closer look around, you wonder if the rumor is a fallacy." );
+	printf( "But since you've already arrived. You decide to walk around. Maybe something will pop up unexpectedly." );
+	printf( "After much sightseeing and walking, you stumble upon a cave." );
+}
