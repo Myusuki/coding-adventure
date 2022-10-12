7c12aeb20ccc48864f9abd797ead39a7aae52e48
Myusuki <myusuki_86666@tutanota.com>
2022-10-12 17:33:22 -0400

    New lined sentences for readibility

--- c/TextAdventure/main.c
+++ c/TextAdventure/main.c
@@ -5,31 +5,31 @@
 int main(int argc, char *argv[])
 {
 	// Intro
-	printf( "Beep! Beep! Beep!" );
-	printf( "It's a Saturday morning. You wake up, reluctant to get out of bed." );
-	printf( "The sun shines brightly through your berdroom window." );
-	printf( "Going down stairs you go perform your same old boring morning routine." );
-	printf( "As usual, every thing is done with no fuss and ease." );
-	printf( "You go back to your room to finish your homework that you've pushed off for today." );
-	printf( "By noon you finish your homework and had lunch. With nothing to do you decide to go out and bike." );
+	printf( "Beep! Beep! Beep!\n" );
+	printf( "It's a Saturday morning. You wake up, reluctant to get out of bed.\n" );
+	printf( "The sun shines brightly through your berdroom window.\n" );
+	printf( "Going down stairs you go perform your same old boring morning routine.\n" );
+	printf( "As usual, every thing is done with no fuss and ease.\n" );
+	printf( "You go back to your room to finish your homework that you've pushed off for today.\n" );
+	printf( "By noon you finish your homework and had lunch. With nothing to do you decide to go out and bike.\n" );
 
-	printf( "Going to the park, you bike around with the wind blowing in your face pleasantly." );
-	printf( "Not feeling the want to bike anymore, you seat yourself at a nearby bench to rest." );
-	printf( "You bathe in the sun, enjoying yourself." );
-	printf( "2 ladies walk by, speaking of some rumor." );
-	printf( "You hear them talk about strange things happening at mountain." );
-	printf( "Being the daredevil you are, you decide tomorrow to go explore exactly what's going on." );
-
-	printf( "Beep! Beep! Beep!" );
-	printf( "It's Sunday morning. Today is somewhat cloudy." );
-	printf( "You get out of bed, eager to go explore the mountain. You perform your same old boring morning routine with haste." );
-	printf( "With the morning routine out of the way, you start packing your backpack meticulously. Prepping little but well enough for your spontaneous journey." );
-	printf( "Arriving at the mountain by bike, nothing seems to be out of the ordinary." );
-	printf( "Taking a closer look around, you wonder if the rumor was no more than a fallacy." );
-	printf( "But since you've already arrived. You decide to walk around. Maybe something will pop up unexpectedly." );
-	printf( "After much sightseeing and walking, you stumble upon a cave." );
+	printf( "Going to the park, you bike around with the wind blowing in your face pleasantly.\n" );
+	printf( "Not feeling the want to bike anymore, you seat yourself at a nearby bench to rest.\n" );
+	printf( "You bathe in the sun, enjoying yourself.\n" );
+	printf( "2 ladies walk by, speaking of some rumor.\n" );
+	printf( "You hear them talk about strange things happening at mountain.\n" );
+	printf( "Being the daredevil you are, you decide tomorrow to go explore exactly what's going on.\n" );
+	printf( "\n" );
+	printf( "Beep! Beep! Beep!\n" );
+	printf( "It's Sunday morning. Today is somewhat cloudy.\n" );
+	printf( "You get out of bed, eager to go explore the mountain. You perform your same old boring morning routine with haste.\n" );
+	printf( "With the morning routine out of the way, you start packing your backpack meticulously. Prepping little but well enough for your spontaneous journey.\n" );
+	printf( "Arriving at the mountain by bike, nothing seems to be out of the ordinary.\n" );
+	printf( "Taking a closer look around, you wonder if the rumor was no more than a fallacy.\n" );
+	printf( "But since you've already arrived. You decide to walk around. Maybe something will pop up unexpectedly.\n" );
+	printf( "After much sightseeing and walking, you stumble upon a cave.\n" );
 	
-	printf( "As there's nothing unusual around the mountain, you shrug your shoulders, grab your flashlight and enter the cave." );
+	printf( "As there's nothing unusual around the mountain, you shrug your shoulders, grab your flashlight and enter the cave.\n" );
 
 	// Dungeon number 1
 	Dungeon1();
