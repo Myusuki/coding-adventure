8b38c474526e9ff8eba9793e9dc08e1b63b9de5f
Myusuki <myusuki_86666@tutanota.com>
2022-10-19 07:56:47 -0400

    Learning how to take user input of a line

--- c/MultiWordInputTest/main.c
+++ c/MultiWordInputTest/main.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char *argv[])
 {
-	
+	char name[100];
+	printf( "Please enter your full name: " );
+	//scanf( "%[^\n]", name );
+	gets( name );
+	printf( "Your name is: %s\n", name );
+
+
 	return 0;
 }
