29c4481bc80a67413a3dfd60e9e6f21da6e749ac
Myusuki <myusuki_86666@tutanota.com>
2023-03-22 10:03:56 -0400

    Finished the last project exercise for Chapter 1

--- c/CProgramming_AModernApproach/Projects/Chapter1/Project8/Project8.c
+++ c/CProgramming_AModernApproach/Projects/Chapter1/Project8/Project8.c
@@ -2,11 +2,30 @@
 
 int main()
 {
-	double percentage;
-	printf( "Enter a percentage: " );
-	scanf( "%lf", &percentage );
+	// using P( 1 + rate/ n ) - payment 
+	float loan = 0;
+	printf( "Enter amount of loan: " );
+	scanf( "%f", &loan );
+
+	float interestRate = 0;
+	printf( "Enter interest rate: " );
+	scanf( "%f", &interestRate );
+
+	float monthlyPayment = 0;
+	printf( "Enter monthly payment: " );
+	scanf( "%f", &monthlyPayment );
+
+	loan *= 1 + ( ( interestRate / 100)/ 12 );
+	loan -= monthlyPayment;
+	printf( "Balance remaining after first payment: %.2f\n", loan );
+
+	loan *= 1 + ( ( interestRate / 100)/ 12 );
+	loan -= monthlyPayment;
+	printf( "Balance remaining after second payment: %.2f\n", loan );
 	
-	double percentTodec = percentage / 100;
-	printf( "%f\n", percentTodec );
+	loan *= 1 + ( ( interestRate / 100)/ 12 );
+	loan -= monthlyPayment;
+	printf( "Balance remaining after third payment: %.2f\n", loan );
+
 	return 0;
 }
