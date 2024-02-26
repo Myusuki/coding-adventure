e109a2ef513b4e22a8bee71668cc329c734a6383
Myusuki <myusuki_86666@tutanota.com>
2024-02-25 19:19:16 -0500

    Used structs for players, cleaned up main func, defined player name processing func

--- c/RiichiMahjongCompass/main.c
+++ c/RiichiMahjongCompass/main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-void NameCheckAndProcess(char name[]);
+struct player
+{
+	char name[27];
+	long int points;
+};
+
+// processes name of players
+void NameProcessor( char name[], int playerNum );
+
+// checks name of players against each other to prevent duplicates 
+bool NameCheck( char name[], char playerArr );
 
 int main(void)
 {
-	// Player info
-	long int player1Points = 0, player2Points = 0, player3Points = 0, player4Points = 0;
-	int maxNameLength = 25;
-	char player1Name[maxNameLength], player2Name[maxNameLength], player3Name[maxNameLength], player4Name[maxNameLength];
+	// Players 
+	struct player player1, player2, player3, player4;
+
+	int maxNameLength = 27;
 
 	// Game info
 	long int startingScore = 0, targetScore = 0;
@@ -22,41 +33,52 @@ int main(void)
 
 	// Ask for player names if no name entered then just use Player 1, 2, etc
 	printf( "Name for player 1 (press Enter for default name): " );
-	fgets(player1Name, maxNameLength, stdin);
-	if( strcmp(player1Name, "\n") == 0 )
-		strncpy(player1Name, "Player 1", 25);
+	fgets(player1.name, maxNameLength, stdin);
+	NameProcessor(player1.name, 1);
 	printf( "Name for player 2 (press Enter for default name): " );
-	fgets(player2Name, maxNameLength, stdin);
-	if( strcmp(player2Name, "\n") == 0 )
-		strncpy(player2Name, "Player 2", 25);
+	fgets(player2.name, maxNameLength, stdin);
+	NameProcessor(player2.name, 2);
 	printf( "Name for player 3 (press Enter for default name): " );
-	fgets(player3Name, maxNameLength, stdin);
-	if( strcmp(player3Name, "\n") == 0 )
-		strncpy(player3Name, "Player 3", 25);
+	fgets(player3.name, maxNameLength, stdin);
+	NameProcessor(player3.name, 3);
 	printf( "Name for player 4 (press Enter for default name): " );
-	fgets(player4Name, maxNameLength, stdin);
-	if( strcmp(player4Name, "\n") == 0 )
-		strncpy(player4Name, "Player 4", 25);
+	fgets(player4.name, maxNameLength, stdin);
+	NameProcessor(player4.name, 4);
 
 	// Assign starting score to players 
-	player1Points = player2Points = player3Points = player4Points = startingScore;
+	player1.points = player2.points = player3.points = player4.points = startingScore;
 	
-	// get rid of newline character in names and replace with null byte
-	if( player1Name[strlen(player1Name) - 1] == '\n' )
-		player1Name[strlen(player1Name) - 1] = '\0';
-	if( player2Name[strlen(player2Name) - 1] == '\n' )
-		player2Name[strlen(player2Name) - 1] = '\0';
-	if( player3Name[strlen(player3Name) - 1] == '\n' )
-		player3Name[strlen(player3Name) - 1] = '\0';
-	if( player4Name[strlen(player4Name) - 1] == '\n' )
-		player4Name[strlen(player4Name) - 1] = '\0';
-
 	// display name and score
-	printf( "%s: %ld\n", player1Name, player1Points );
-	printf( "%s: %ld\n", player2Name, player2Points );
-	printf( "%s: %ld\n", player3Name, player3Points );
-	printf( "%s: %ld\n", player4Name, player4Points );
+	printf( "%s: %ld\n", player1.name, player1.points );
+	printf( "%s: %ld\n", player2.name, player2.points);
+	printf( "%s: %ld\n", player3.name, player3.points );
+	printf( "%s: %ld\n", player4.name, player4.points );
 
 	return 0;
 }
 
+void NameProcessor(char name[], int playerNum )
+{
+	// Check if player wants default name
+	if( strncmp(name, "\n", 1) == 0 )
+	{
+		switch( playerNum )
+		{
+			case 1:
+				strcpy(name, "Player 1");
+			break;
+			case 2:
+				strcpy(name, "Player 2");
+			break;
+			case 3:
+				strcpy(name, "Player 3");
+			break;
+			case 4:
+				strcpy(name, "Player 4");
+			break;
+		}
+	}
+	// Change newline byte at end of string to null byte
+	if( name[strlen(name) - 1] == '\n' )
+		name[strlen(name) - 1] = '\0';
+}
