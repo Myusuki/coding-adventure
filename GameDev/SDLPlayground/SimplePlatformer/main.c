fd5b9a89acaa8e5f0ec6f052fe11d7b10430ccef
Myusuki <myusuki_86666@tutanota.com>
2024-10-05 17:56:36 -0400

    Starting SimplePlatformer from scratch

--- GameDev/SDLPlayground/SimplePlatformer/main.c
+++ GameDev/SDLPlayground/SimplePlatformer/main.c
@@ -1,20 +1,95 @@
-#include <SDL2/SDL.h>
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <time.h>
 
-const int screenWidth = 640;
-const int screenHeight = 480;
+// global variables
+#define screenWidth 800
+#define screenHeight 600
+
+// app struct
+typedef struct App 
+{
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+} App;
+
+// game struct
+App Game = {0};
+
+// global modular functions
+bool Init();
+void Quit();
 
 int main( int argc, char *argv[] )
 {
-  // window to be rendered to 
-  SDL_Window* window = NULL;
-  // surface contained by the window 
-  SDL_Surface* screenSurface = NULL;
+  // init program
+  if( !Init() )
+  {
+    printf( "Failed to initialize!\n" );
+    return 1;
+  }
 
-  if( SDL_Init(SDL_INIT_VIDEO) < 0 )
-    printf( "SDL could not be initialized! SDL Error: %s\n", SDL_GetError() );
+  bool quit = false;
+  SDL_Event event;
 
-  
+  // game loop
+  while( !quit )
+  {
+    // poll for input events
+    while( SDL_PollEvent(&event) > 0 )
+    {
+      if( event.type == SDL_QUIT )
+        quit = true;
+    }
+    // draw stuff to screen
+    SDL_RenderClear(Game.renderer);
+    SDL_SetRenderDrawColor(Game.renderer, 0xff, 0xff, 0xff, 0xff);
+
+    SDL_RenderPresent(Game.renderer);
+  }
 
   return 0;
+}
+
+bool Init()
+{
+  // init sdl
+  if(SDL_Init(SDL_INIT_VIDEO) < 0 )
+  {
+    printf( "SDL failed to initialize! SDL Error: %s\n", SDL_GetError() );
+    return false;
+  }
+
+  // init Game struct
+  Game.window = SDL_CreateWindow("Simple Platformer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+  if( Game.window == NULL )
+  {
+    printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+    return false;
+  }
+  Game.renderer = SDL_CreateRenderer(Game.window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if( Game.renderer == NULL )
+  {
+    printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+    return false;
+  }
+  SDL_SetRenderDrawColor(Game.renderer, 0xff, 0xff, 0xff, 0xff);
+
+  return true;
+}
+void Quit()
+{
+  // destroy renderer and window
+  SDL_DestroyRenderer(Game.renderer);
+  Game.renderer = NULL;
+  SDL_DestroyWindow(Game.window);
+  Game.window = NULL;
+
+  // quit sdl
+  SDL_Quit();
 }
