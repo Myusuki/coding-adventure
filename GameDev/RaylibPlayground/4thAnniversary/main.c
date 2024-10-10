9ee601d292c123dc52d9ce04f68c6ed1d0cf15c8
Myusuki <myusuki_86666@tutanota.com>
2024-10-09 21:38:50 -0400

    First anniversary 'game'; Will improve it down the line

---
+++ GameDev/RaylibPlayground/4thAnniversary/main.c
@@ -0,0 +1,62 @@
+#include <raylib.h>
+#include <stdbool.h>
+
+// global variables
+#define screenWidth 1920
+#define screenHeight 1080
+
+// game screen enums
+typedef enum GameScreen{ MENU = 0, GAMEPLAY } GameScreen;
+
+int main(void)
+{
+  SetConfigFlags(FLAG_VSYNC_HINT);
+  InitWindow(screenWidth, screenHeight, "Simple Platformer");
+  SetTargetFPS(60);
+
+  GameScreen currentScreen = MENU;
+  bool showText = false;
+  int framesCounter = 0;
+
+  while( !WindowShouldClose() || IsKeyPressed(KEY_ESCAPE) )
+  {
+    switch( currentScreen )
+    {
+      case MENU:
+        framesCounter++;
+
+        if( IsKeyPressed(KEY_ENTER) )
+        {
+          showText = true;
+          currentScreen = GAMEPLAY;
+        }
+        break;
+      case GAMEPLAY:
+        framesCounter++;
+        break;
+      default:
+        break;
+    }
+
+    BeginDrawing();
+      ClearBackground(WHITE);
+      switch( currentScreen )
+      {
+        case MENU:
+          if( (framesCounter / 30) % 2 == 0 )
+            DrawText("PRESS ENTER", (screenWidth - MeasureText("PRESS ENTER", 50) ) / 2, (screenHeight / 2), 50, BLACK);
+          break;
+        case GAMEPLAY:
+          if( showText )
+            DrawText("Happy ANNIVERSARY!!!", (screenWidth - MeasureText("Happy ANNIVERSARY!", 50) ) / 2 , (screenHeight / 2), 50, DARKPURPLE);
+          break;
+        default:
+          break;
+      }
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
+}
