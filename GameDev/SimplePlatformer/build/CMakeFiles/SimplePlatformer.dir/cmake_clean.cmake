5dce64e6cf95f251274419bc2b72bd78347427c3
Myusuki <myusuki_86666@tutanota.com>
2024-11-24 09:51:13 -0500

    Trying out how to use a tilemap

---
+++ GameDev/SimplePlatformer/build/CMakeFiles/SimplePlatformer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimplePlatformer.dir/link.d"
+  "CMakeFiles/SimplePlatformer.dir/main.c.o"
+  "CMakeFiles/SimplePlatformer.dir/main.c.o.d"
+  "SimplePlatformer"
+  "SimplePlatformer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SimplePlatformer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
