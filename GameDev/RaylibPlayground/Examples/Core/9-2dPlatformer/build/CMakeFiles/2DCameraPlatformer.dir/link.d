d25606958eca2a4155e46415fea19d1851badab3
Myusuki <myusuki_86666@tutanota.com>
2024-12-03 21:06:12 -0500

    Learning to make a 2d platformer through this example

---
+++ GameDev/RaylibPlayground/Examples/Core/9-2dPlatformer/build/CMakeFiles/2DCameraPlatformer.dir/link.d
@@ -0,0 +1,49 @@
+/home/myu/Gitdownloads/coding-adventure/GameDev/RaylibPlayground/Examples/Core/9-2dPlatformer/2DCameraPlatformer: \
+  /usr/lib/libm.so \
+  /usr/lib/libm.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libc.so \
+  /usr/lib/libc.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so.1 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib32/libxcb.so.1 \
+  /usr/lib/libxcb.so.1 \
+  /usr/lib32/libXau.so.6 \
+  /usr/lib/libXau.so.6 \
+  /usr/lib32/libXdmcp.so.6 \
+  /usr/lib/libXdmcp.so.6
+
+/usr/lib/libm.so:
+
+/usr/lib/libm.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libc.so:
+
+/usr/lib/libc.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so.1:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib32/libxcb.so.1:
+
+/usr/lib/libxcb.so.1:
+
+/usr/lib32/libXau.so.6:
+
+/usr/lib/libXau.so.6:
+
+/usr/lib32/libXdmcp.so.6:
+
+/usr/lib/libXdmcp.so.6:
