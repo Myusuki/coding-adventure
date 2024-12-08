87167561969de07bbb0bb9f82d5f4290435a285c
Myusuki <myusuki_86666@tutanota.com>
2024-12-08 16:01:19 -0500

    Implemented a bounded 2d camera to the tilemap so it's possible to view within a tilemap; will be archiving code to somewhere else

--- GameDev/SimplePlatformer/main.c
+++ GameDev/SimplePlatformer/main.c
@@ -77,12 +77,12 @@ void draw_image_layer(tmx_image *image)
 	DrawTexture(*texture, 0, 0, WHITE);
 }
 
-void draw_tile(void *image, unsigned int sx, unsigned int sy, unsigned int sw, unsigned int sh,
-               unsigned int dx, unsigned int dy, float opacity, unsigned int flags)
+void draw_tile(void *image, unsigned int sourceX, unsigned int sourceY, unsigned int sourceWidth, unsigned int sourceHeight,
+               unsigned int destX, unsigned int destY, float opacity, unsigned int flags)
 {
     Texture2D *texture = (Texture2D*)image;
     int op = 0xFF * opacity;
-    DrawTextureRec(*texture, (Rectangle) {sx, sy, sw, sh}, (Vector2) {dx, dy}, (Color) {op, op, op, op});
+    DrawTextureRec(*texture, (Rectangle) {sourceX, sourceY, sourceWidth, sourceHeight}, (Vector2) {destX, destY}, (Color) {op, op, op, op});
 }
 
 void draw_layer(tmx_map *map, tmx_layer *layer)
@@ -105,13 +105,13 @@ void draw_layer(tmx_map *map, tmx_layer *layer)
 				w  = ts->tile_width;
 				h  = ts->tile_height;
 				if (im) {
-                    image = im->resource_image;
+          image = im->resource_image;
 				}
 				else {
-                    image = ts->image->resource_image;
+          image = ts->image->resource_image;
 				}
 				flags = (layer->content.gids[(i*map->width)+j]) & ~TMX_FLIP_BITS_REMOVAL;
-                draw_tile(image, x, y, w, h, j*ts->tile_width, i*ts->tile_height, op, flags);
+        draw_tile(image, x, y, w, h, j*ts->tile_width, i*ts->tile_height, op, flags);
 			}
 		}
 	}
@@ -119,21 +119,27 @@ void draw_layer(tmx_map *map, tmx_layer *layer)
 
 void draw_all_layers(tmx_map *map, tmx_layer *layers)
 {
-	while (layers) {
-		if (layers->visible) {
-
-			if (layers->type == L_GROUP) {
-				draw_all_layers(map, layers->content.group_head); // recursive call
-			}
-			else if (layers->type == L_OBJGR) {
-				draw_objects(layers->content.objgr);
-			}
-			else if (layers->type == L_IMAGE) {
-				draw_image_layer(layers->content.image);
-			}
-			else if (layers->type == L_LAYER) {
-				draw_layer(map, layers);
-			}
+	while (layers)
+  {
+		if (layers->visible)
+    {
+      switch( layers->type )
+      {
+        case L_GROUP:
+          draw_all_layers(map, layers->content.group_head);
+          break;
+        case L_OBJGR:
+          draw_objects(layers->content.objgr);
+          break;
+        case L_IMAGE:
+          draw_image_layer(layers->content.image);
+          break;
+        case L_LAYER:
+          draw_layer(map, layers);
+          break;
+        default:
+          break;
+      }
 		}
 		layers = layers->next;
 	}
@@ -154,6 +160,7 @@ int main(int argc, char **argv)
 	}
 
 	SetTargetFPS(30);
+  SetExitKey(KEY_NULL);
 
 	/* Set the callback globs in the main function */
 	tmx_img_load_func = raylib_tex_loader;
@@ -165,9 +172,47 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+  Camera2D camera = { .target = (Vector2){ ( (float)(map->tile_width * map->width) / 2), ( (float)(map->tile_height * map->height) / 2) },
+                      .offset = (Vector2){ ( (float)DISPLAY_W / 2), ( (float)DISPLAY_H / 2) },
+                      .zoom = 3.0 };
+
 	while (!WindowShouldClose()) {
+    // input handling
+    // movement
+    Vector2 worldToScreenPos = GetScreenToWorld2D( (Vector2){ ( (float)DISPLAY_W / 2), ( (float)DISPLAY_H / 2) }, camera);
+    if( (worldToScreenPos.x - ( (float)DISPLAY_W / 2) ) > 0 )
+    {
+      if( IsKeyDown(KEY_LEFT) )
+        camera.target.x -= 10;
+    }
+    if( (worldToScreenPos.x + ( (float)DISPLAY_W / 2) ) < (map->tile_width * map->height) )
+    {
+      if( IsKeyDown(KEY_RIGHT) )
+        camera.target.x += 10;
+    }
+    if( (worldToScreenPos.y - ( (float)DISPLAY_H / 2) ) > 0 )
+    {
+      if( IsKeyDown(KEY_UP) )
+        camera.target.y -= 10;
+    }
+    if( (worldToScreenPos.y + ( (float)DISPLAY_H / 2) ) < (map->tile_height * map->height) )
+    {
+      if( IsKeyDown(KEY_DOWN) )
+        camera.target.y +=10;
+    }
+    // reset
+    if( IsKeyPressed(KEY_R) )
+      camera.target = (Vector2){ (float)(map->tile_width * map->width) / 2, (float)(map->tile_height * map->height) / 2};
+
+    // logic
+    // Vector2 screenToWorldPos = GetWorldToScreen2D( (Vector2){0, 0}, camera);
+    // printf( "X: %f, Y:%f\n", screenToWorldPos.x, screenToWorldPos.y );
+    printf( "X: %f, Y: %f\n", worldToScreenPos.x, worldToScreenPos.y );
+
 		BeginDrawing();
-		render_map(map);
+      BeginMode2D(camera);
+        render_map(map);
+      EndMode2D();
 		EndDrawing();
 	}
 
